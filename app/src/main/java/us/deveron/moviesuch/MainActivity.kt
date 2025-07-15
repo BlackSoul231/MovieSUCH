ef86dc43217bc87bb7828dbc6db16466f01e20ef
derTeufel <nik231as@gmail.com>
2025-07-15 12:17:39 +0300

    P.1 Working fine. For further development

--- app/src/main/java/us/deveron/moviesuch/MainActivity.kt
+++ app/src/main/java/us/deveron/moviesuch/MainActivity.kt
@@ -6,20 +6,42 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import us.deveron.moviesuch.databinding.ActivityMainBinding
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val knopka = findViewById<Button>(R.id.button5)
-        knopka.setOnClickListener {
-            Toast.makeText(this, "Perdin!", Toast.LENGTH_SHORT).show()
-        }
+        initMenuButtons()
     }
 
     fun onClickToast(view: View) {
         Toast.makeText(this, "Chin!", Toast.LENGTH_SHORT).show()
+    }
+    fun initMenuButtons() {
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        button1.setOnClickListener {
+            Toast.makeText(this, "Continue", Toast.LENGTH_SHORT).show()
+        }
+        button2.setOnClickListener {
+            Toast.makeText(this, "New", Toast.LENGTH_SHORT).show()
+        }
+        button3.setOnClickListener {
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show()
+        }
+        button4.setOnClickListener {
+            Toast.makeText(this, "Additional", Toast.LENGTH_SHORT).show()
+        }
+        button5.setOnClickListener {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+        }
+        button6.setOnClickListener {
+            Toast.makeText(this, "Leave", Toast.LENGTH_SHORT).show()
+        }
     }
 }
